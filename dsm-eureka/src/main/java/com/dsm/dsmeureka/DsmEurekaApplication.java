4ca1d435c6d67cddca3ae3fc5a0dfaee25bff8c5
malin <malin@thunisft.com>
2018-12-15 15:07:17 +0800

    feat: 注册中心提交

---
+++ dsm-eureka/src/main/java/com/dsm/dsmeureka/DsmEurekaApplication.java
@@ -0,0 +1,14 @@
+package com.dsm.dsmeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsmEurekaApplication.class, args);
+    }
+
+}
+
