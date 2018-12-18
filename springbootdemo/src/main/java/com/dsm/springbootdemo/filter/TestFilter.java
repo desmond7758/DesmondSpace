39b75cd70766f4cfc1db1a79f02982f2a9468211
desmond <desmond7758@qq.com>
2018-12-18 20:46:04 +0800

    feat: Spring Boot Demo 提交

--- springbootdemo/src/main/java/com/dsm/springbootdemo/filter/TestFilter.java
+++ springbootdemo/src/main/java/com/dsm/springbootdemo/filter/TestFilter.java
@@ -1,4 +1,23 @@
 package com.dsm.springbootdemo.filter;
 
-public class TestFilter {
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+public class TestFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.err.println("过滤完毕");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
 }
--- springbootdemo/src/main/java/com/dsm/springbootdemo/servlet/TestServlet.java
+++ springbootdemo/src/main/java/com/dsm/springbootdemo/servlet/TestServlet.java
@@ -1,4 +1,35 @@
 package com.dsm.springbootdemo.servlet;
 
-public class TestServlet {
+import ch.qos.logback.core.util.TimeUtil;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.AsyncContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+@WebServlet(urlPatterns = "/api/dsm", asyncSupported = true)
+public class TestServlet extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 异步
+        AsyncContext asyncContext = req.startAsync();
+        // 开启异步 runnable
+        asyncContext.start(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.err.println("异步开始啦");
+        });
+        // 这行不加 页面会一直转圈 不返回
+        asyncContext.complete();
+
+        resp.getWriter().write("Hello Word");
+    }
 }
