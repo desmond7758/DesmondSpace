24935e1d5635ed0fac6dfbfa8b2464b82eed6392
Desmond <274614959@qq.com>
2019-10-22 22:27:31 +0800

    项目首次提交

---
+++ mailbox/src/test/java/com/teemo/mailbox/MailboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teemo.mailbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
